929f7612b12abc75ee97c09b733f76a3d16d44ba
Joel Gomes da Silva <joelgomes1994@hotmail.com>
2021-04-06 15:39:18 -0300

    Added GameJolt logo to docs.

--- docs/conf.py
+++ docs/conf.py
@@ -45,6 +45,7 @@ master_doc = 'index'
 # a list of builtin themes.
 #
 html_theme = 'sphinx_rtd_theme'
+html_logo = 'img/logo.png' 
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
