4bc98be527b8641f94eebfee69ad92c07cd6937a
Joel Gomes da Silva <joelgomes1994@hotmail.com>
2021-04-06 13:15:02 -0300

    Added base files for pypi publishing. Work in progress.

---
+++ README.rst
@@ -0,0 +1,6 @@
+Gamejolt API
+------------
+
+.. image:: https://readthedocs.org/projects/gamejolt/badge/?version=latest
+
+A Python module for interacting with the GameJolt API.
---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+with open('README.md') as f:
+    readme = f.read()
+
+setup(name="gamejoltapi",
+      version="1",
+      author="Joel Gomes da Silva",
+      author_email="joelgomes1994@hotmail.com",
+      description="Single threaded Python interface for the GameJolt API running through HTTP requests",
+      license="MIT",
+      keywords="game gamedev api interface gamejolt gamejoltapi",
+      url="https://github.com/bgempire/gamejoltapi",
+      long_description=readme,
+      py_modules=['gamejoltapi'],
+      classifiers=["Development Status :: 5 - Production/Stable",
+                   "Intended Audience :: Developers",
+                   "License :: OSI Approved :: MIT License",
+                   "Operating System :: OS Independent",
+                   "Programming Language :: Python :: 3 :: Only",
+                   "Topic :: Games/Entertainment",
+                   "Topic :: Software Development :: Libraries"])
