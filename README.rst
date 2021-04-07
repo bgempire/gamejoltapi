27e55ad3876f9c5d4190111d164319ce07815d44
Joel Gomes da Silva <joelgomes1994@hotmail.com>
2021-04-07 09:36:33 -0300

    Added pypi pipeline

--- README.rst
+++
@@ -1,6 +0,0 @@
-Gamejolt API
-------------
-
-.. image:: https://readthedocs.org/projects/gamejolt/badge/?version=latest
-
-A Python module for interacting with the GameJolt API.
--- setup.py
+++ setup.py
@@ -5,13 +5,14 @@ with open('README.md') as f:
     readme = f.read()
 
 setup(name="gamejoltapi",
-      version="1",
+      version="0.0.1",
       author="Joel Gomes da Silva",
       author_email="joelgomes1994@hotmail.com",
       description="Single threaded Python interface for the GameJolt API running through HTTP requests",
       license="MIT",
       keywords="game gamedev api interface gamejolt gamejoltapi",
       url="https://github.com/bgempire/gamejoltapi",
+      long_description_content_type = "text/markdown",
       long_description=readme,
       py_modules=['gamejoltapi'],
       classifiers=["Development Status :: 5 - Production/Stable",
